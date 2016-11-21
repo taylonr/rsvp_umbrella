f87a63ae8aa2df26d42b3717119c2a007265f105
Nate Taylor <taylonr@gmail.com>
2016-11-21 16:13:46 -0600

    Creating rsvp application.

--- apps/rsvp/mix.exs
+++ apps/rsvp/mix.exs
@@ -18,7 +18,8 @@ defmodule Rsvp.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger],
+     mod: {Rsvp, []}]
   end
 
   # Dependencies can be Hex packages:
