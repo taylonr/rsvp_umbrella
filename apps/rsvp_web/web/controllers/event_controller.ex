7f66edd21c2beec9ebda4163a91a7bb5d3c0a7da
Nate Taylor <taylonr@gmail.com>
2016-11-29 20:48:48 -0600

    Creating the show function.

--- apps/rsvp_web/web/controllers/event_controller.ex
+++ apps/rsvp_web/web/controllers/event_controller.ex
@@ -1,3 +1,7 @@
 defmodule RsvpWeb.EventController do
     use RsvpWeb.Web, :controller
+
+    def show(conn, params) do
+        render conn, "details.html", event: "Events #{params["id"]}"
+    end
 end
