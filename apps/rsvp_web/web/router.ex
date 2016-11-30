28d8505e53c9168caa4b03fd48c090bd4be1c86f
Nate Taylor <taylonr@gmail.com>
2016-11-29 20:01:33 -0600

    Created events route.

--- apps/rsvp_web/web/router.ex
+++ apps/rsvp_web/web/router.ex
@@ -17,6 +17,7 @@ defmodule RsvpWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/events/:id", EventController, :show
   end
 
   # Other scopes may use custom stacks.
