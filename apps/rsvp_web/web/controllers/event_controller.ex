def9628929d63e8d26b7f829afab085278888517
Nate Taylor <taylonr@gmail.com>
2016-12-20 13:02:25 -0600

    Adding list view.

--- apps/rsvp_web/web/controllers/event_controller.ex
+++ apps/rsvp_web/web/controllers/event_controller.ex
@@ -5,4 +5,9 @@ defmodule RsvpWeb.EventController do
         event = Rsvp.EventQueries.get_by_id(params["id"])
         render conn, "details.html", event: event
     end
+
+    def list(conn, _params) do
+        events = Rsvp.EventQueries.get_all
+        render conn, "list.html", events: events
+    end
 end
